23e40fa769449f8c8d702f48b231293bd5b4086a
belarge <bruce.large@gmail.com>
2015-03-19 18:59:13 +1000

    Assignment Push 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## we can improve the performance of the computation when applied in looping 
 ## functions do
 
-## This function creates a special list to pack the Matrix Inverse Function
+## This function creates a special list to pack the Matrix Inverse Functions
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -10,9 +10,13 @@ makeCacheMatrix <- function(x = matrix()) {
 		x <<- y
 		inv <<- NULL
 	}
-	get <- function() x
-	setInv <- function(MatrixInv) inv <<- MatrixInv 
+	##This sets the matrix and clears the inverse variable
+	get <- function() x 
+	##This simply returns the matrix variable
+	setInv <- function(MatrixInv) inv <<- MatrixInv
+	## This simply sets the matrix inverse that has been computed externally
 	getInv <- function() inv
+	## This simply returns the precomputed inverse
 	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
@@ -26,9 +30,14 @@ cacheSolve <- function(x, ...) {
 			message("Retrieving Cached Inverse Matrix")
 			return(inv)
 		}
+		## this block checks to see whether we have already created the inverse - if so
+		## we will indicate we have cached and then return the inverse matrix
 		data <- x$get()
 		inv <- solve(data, ...)
 		x$setInv(inv)
 		inv
-		
+		## If we haven't computed the inverse, we recover the matrix
+		## then we compute the inverse matrix using solve
+		## we then set the inverse in the cached variable which is checked in future computations
+		## then we return the inverse matrix
 }
